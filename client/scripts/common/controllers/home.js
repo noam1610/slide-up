6819ca606dd90b555f469694c27b84328c928ea1
noam1610 <nbenlolo@yoobic.com>
2016-08-15 17:43:08 +0200

    WIP

--- client/scripts/common/controllers/home.js
+++ client/scripts/common/controllers/home.js
@@ -1,19 +1,49 @@
 'use strict';
 var controllername = 'home';
+
 // var animateCss = require('animate.css-js');
 
 module.exports = function(app) {
     var fullname = app.name + '.' + controllername;
     /*jshint validthis: true */
 
-    var deps = [];
+    var deps = ['$animate'];
 
-    function controller() {
+    function controller($animate) {
         var vm = this;
         vm.controllername = fullname;
         console.log('I exist');
 
         vm.boolean = false;
+        console.log($animate);
+
+        $animate.animate('noam', function() {
+            document.getElementById('noam').velocity('fadeIn', {
+                delay: 300,
+                duration: 600
+            });
+            return {
+                enter: function(element, done) {
+                    element.velocity('fadeIn', {
+                        delay: 300,
+                        duration: 600,
+                        complete: done
+                    });
+                    return function() {
+                        element.stop();
+                    };
+                },
+                leave: function(element, done) {
+                    element.velocity('fadeOut', {
+                        duration: 300,
+                        complete: done
+                    });
+                    return function() {
+                        element.stop();
+                    };
+                }
+            };
+        });
 
         // var element = document.getElementById('noam');
         // animateCss.show(element, {
